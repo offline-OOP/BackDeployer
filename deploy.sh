59279e45c13c506081bb19b2ef101b1be4a2f39f
Vladimir Gololobov <Vladimir.Gololobov@x5.ru>
2021-10-10 18:26:00 +0300

    feat(): user added

--- deploy.sh
+++ deploy.sh
@@ -26,5 +26,5 @@ log "${GREEN} Logging to docker repo ${CLOSE}"
 docker login -u "$CI_REPOSITORY_NAME" -p "$CI_REPOSITORY_PASSWORD"
 
 log "${GREEN} Deploying app ${CLOSE}"
-ID_U=$(id -u) ID_G=$(id -g) NEO4J_PASSWORD=$NEO4J_PASSWORD REDIS_PASSWORD=$REDIS_PASSWORD docker stack deploy -c stack/db/stack.yml app-db-stack
+ID_U=$(sudo id -u) ID_G=$(sudo id -g) NEO4J_PASSWORD=$NEO4J_PASSWORD REDIS_PASSWORD=$REDIS_PASSWORD docker stack deploy -c stack/db/stack.yml app-db-stack
 docker stack deploy -c stack/app/stack.yml app-stack
